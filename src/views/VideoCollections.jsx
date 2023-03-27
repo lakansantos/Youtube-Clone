a1ca4c66df291a1c5cad89dd7f386efbb8e2c425
ITW <rizesuoracle25@gmail.com>
2023-03-28 07:36:09 +0800

    finished building UI for video collecitons

--- src/views/VideoCollections.jsx
+++ src/views/VideoCollections.jsx
@@ -1,22 +1,61 @@
 import axios from "axios";
 import { useEffect, useState} from "react";
+import {AiFillCheckCircle} from 'react-icons/ai'
+import {BsDot} from 'react-icons/bs'
 
 const VideoCollections = () => {
     const [data, setData] = useState(null)
 
     useEffect(() => {
         axios.get('/src/data/VideosData.json')
-        .then(response => setData(response.data.videos))
+        .then(response => {
+            setData(response.data.videos)
+            console.log(response.data.videos)
+        })
         .catch(err => console.error(err))
     }, [])
 
     return (
         <div className="video-collections-container">
-            {data?.map((video) => {
+            {data?.map((postData) => {
                 return (
-                    <div className="video-box" key={video.id}>
-                        <div className="thumbnail-container">test</div>
-                        <div className="video-details-container">test</div>
+                    <div className="video-box" key={postData.id}>
+                        <div 
+                        className="thumbnail-container"
+                        style={{
+                            background: `url(${postData.thumbnail}) center/cover no-repeat`}}
+                        >
+                        </div>
+                        <div className="video-details-container">
+                            <div className="channel-profile-container">
+                                <img src={postData.channelProfile} alt={`${postData.channel} avatar`} className="channel-profile"/>
+                            </div>
+                            <div className="post-details-container">
+                                <h3 className="youtube-post-title">{postData.title}</h3>
+                                <p className="post-channel-name">{postData.channel} 
+                                    {postData.verified ? 
+                                        <span>
+                                            <AiFillCheckCircle style={{
+                                                color: 'gray',
+                                                marginLeft: '5px'
+                                            }}/>
+                                        </span> 
+                                        : null
+                                    }
+                                </p>
+                                <div className="viewcount-container">
+                                    <p className="view-count-text">{postData.viewCount} views</p>
+                                    <span>
+                                        <BsDot style={{
+                                            color: 'gray',
+                                            height: '100%',
+                                        }}/>
+                                    </span>
+                                    <p>{postData.time}</p>
+                                </div>
+
+                            </div>
+                        </div>
                     </div>
                 )
             })}
