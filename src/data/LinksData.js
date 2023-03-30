b281aa295fcce96c85a25785f07a757a52890206
ITW <rizesuoracle25@gmail.com>
2023-03-30 12:31:04 +0800

    fixed alignment for linksdata array

--- src/data/LinksData.js
+++ src/data/LinksData.js
@@ -1,1 +1,18 @@
-export const LinksData = ['All', 'Music', 'Live', 'Playlists', 'Love songs', 'NBA', 'Gaming', 'K-pop', 'DJ mix', 'Dramedy', 'Guitar', 'Eating', 'Conversation', 'Adventure Games', 'Recently uploaded', 'New to you'];
+export const LinksData = [
+    'All', 
+    'Music', 
+    'Live', 
+    'Playlists', 
+    'Love songs', 
+    'NBA', 
+    'Gaming', 
+    'K-pop', 
+    'DJ mix', 
+    'Dramedy', 
+    'Guitar', 
+    'Eating', 
+    'Conversation', 
+    'Adventure Games', 
+    'Recently uploaded', 
+    'New to you'
+];
